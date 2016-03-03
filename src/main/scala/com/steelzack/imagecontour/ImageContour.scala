49b318da1f4db7ef4d3e276da605d9faa616dd76
Joao F.S.E <jofisaes@hotmail.com>
2016-03-03 15:11:30 +0100

    SZ-N Iniitial setup

---
+++ src/main/scala/com/steelzack/imagecontour/ImageContour.scala
@@ -0,0 +1,8 @@
+package com.steelzack.imagecontour
+
+/**
+  * Created by joaofilipesabinoesperancinha on 03-03-16.
+  */
+class ImageContour {
+
+}
---
+++ src/test/scala/com/steelzack/imagecontour/ImageContourTest.groovy
@@ -0,0 +1,15 @@
+package com.steelzack.imagecontour
+
+/**
+ * Created by joaofilipesabinoesperancinha on 03-03-16.
+ */
+class ImageContourTest {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void tearDown() {
+
+    }
+}
