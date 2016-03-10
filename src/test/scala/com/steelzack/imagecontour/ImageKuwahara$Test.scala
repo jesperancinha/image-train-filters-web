283e327f6a088121b9d7ec641272ac17eb747e31
Joao F.S.E <jofisaes@hotmail.com>
2016-03-10 09:16:14 +0100

    SZ-N Resource file for Kuwahara

--- src/test/scala/com/steelzack/imagecontour/ImageKuwahara$Test.scala
+++ src/test/scala/com/steelzack/imagecontour/ImageKuwahara$Test.scala
@@ -1,13 +1,30 @@
 package com.steelzack.imagecontour
 
-import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 /**
   * Created by joaofilipesabinoesperancinha on 10-03-16.
   */
-class ImageKuwahara$Test extends FunSuite {
+class ImageKuwahara$Test extends FunSuite with BeforeAndAfterEach {
+
+  override def beforeEach() {
+
+  }
+
+  override def afterEach() {
+
+  }
+
+  test("testGetStandardDeviation") {
+    val imageKuwahara = ImageKuwahara
+    val averageArray = imageKuwahara.getAverage()
+  }
 
   test("testConvertAndSaveImage") {
+
+  }
+
+  test("testGetAverage") {
 
   }
 
