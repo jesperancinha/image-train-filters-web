6e8c31dec63a1932f9eebcbfb89fbd64368c49a9
Joao F.S.E <jofisaes@hotmail.com>
2016-03-11 20:03:52 +0100

    SZ-N Minimum deviation skeleton

--- src/main/scala/com/steelzack/imagecontour/ImageKuwahara.scala
+++ src/main/scala/com/steelzack/imagecontour/ImageKuwahara.scala
@@ -28,25 +28,41 @@ object ImageKuwahara {
     total
   }
 
-  def getStandardDeviation(source: BufferedImage, x1: Int, y1: Int, x2: Int, y2: Int, avg: Array[Double]): Array[Double] = {
+  def getStandardDeviation(source: BufferedImage, x1: Int, y1: Int, x2: Int, y2: Int, avg: Array[Double]): Double = {
     val numberOfPoints: Int = (y2 - y1) * (x2 - x1)
     val arr = Array.fill[Double](3)(0.0)
-    val total = Array.fill[Double](3)(0.0)
+    var total: Double = 0.0
 
     for (i <- y1 to y2) {
       for (j <- x1 to x2) {
         source.getData().getPixel(i, j, arr)
-        total(0) = total(0) + scala.math.pow(arr(0) - avg(0),2)
-        total(1) = total(1) + scala.math.pow(arr(1) - avg(1),2)
-        total(2) = total(2) + scala.math.pow(arr(2) - avg(2),2)
+        val grey: Double = 0.299 * arr(0) + 0.587 * arr(1) + 0.114 * arr(2)
+        total += grey
       }
     }
 
-    total(0) = total(0) / numberOfPoints
-    total(1) = total(1) / numberOfPoints
-    total(2) = total(2) / numberOfPoints
+    total /= numberOfPoints
 
     return total
+  }
+
+  def getMinDeviationAverageColor(avg1: Array[Double], //
+                                  avg2: Array[Double], //
+                                  avg3: Array[Double], //
+                                  avg4: Array[Double], //
+                                  std1: Double, //
+                                  std2: Double, //
+                                  std3: Double, //
+                                  std4: Double //
+                                 ) : Array[Double] = {
+
+    val minValue =  math.min(std1, math.min(std2, math.min(std3, std4)))
+
+    
+
+
+
+  return null
   }
 
   def convertAndSaveImage(source: BufferedImage, //
@@ -72,6 +88,7 @@ object ImageKuwahara {
           val std3 = getStandardDeviation(source, i + squareSize, j - squareSize, i, j, avg3)
           val std4 = getStandardDeviation(source, i + squareSize, j + squareSize, i, j, avg4)
 
+          getMinDeviationAverageColor(avg1,avg2,avg3,avg4,std1,std2,std3,std4)
         }
       }
     }
