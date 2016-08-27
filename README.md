d1cdccd0b490dd17eb0b6e0b21e55ce6a80c78e5
João Esperancinha <jofisaes@hotmail.com>
2016-08-27 21:00:13 +0200

    commands loaded on a final map for processing choice

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ From <img src="https://raw.githubusercontent.com/jesperancinha/image-contour/mas
 ## License
 
 ```
-Copyright 2016-2017 Steelzack
+Copyright 2016-2017 JEsper
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
--- src/main/scala/com/jesper/imagecontour/rest/ImageContourMultiPartDataHandler.scala
+++ src/main/scala/com/jesper/imagecontour/rest/ImageContourMultiPartDataHandler.scala
@@ -13,6 +13,7 @@ import akka.stream.scaladsl.FileIO
 import com.jesper.imagecontour.Boot
 import com.jesper.imagecontour.filters.{ImageContour, ImageKuwahara, ImageManager, ImageSaver}
 
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
 trait ImageContourMultiPartDataHandler {
@@ -30,29 +31,46 @@ trait ImageContourMultiPartDataHandler {
   }
 
   def processMultiPartData: Route = path("images" / Rest) {
-    rest =>
+    restCommand =>
       (post & entity(as[Multipart.FormData])) { formData =>
         complete {
           val log = Logging(system, this)
           //noinspection UnnecessaryPartialFunction
-          val extractedData: Future[Map[String, Any]] = formData.parts.mapAsync[(String, Any)](1) {
-            case bodyPart: BodyPart =>
+
+          val extractedData: Future[Map[String, String]] = formData.parts.mapAsync[(String, String)](1) {
+            case bodyPart: BodyPart if bodyPart.name.equals("filename") =>
               log.info(s"received ${bodyPart.name} file")
+              log.info(s"command $restCommand")
               val tempFile: File = new File(Boot.fileRootSource, bodyPart.filename.orNull)
-              val data = bodyPart.entity.dataBytes.runWith(FileIO.toFile(tempFile)).map(_ => bodyPart.name -> bodyPart.getFilename())
+              val data: Future[(String, String)] = bodyPart
+                .entity
+                .dataBytes
+                .runWith(FileIO.toFile(tempFile)).map(_ => bodyPart.name -> bodyPart.getFilename.orElse(""))
               log.info("saved! - " + tempFile)
-              val srcBuff = ImageManager.getBufferedImage(tempFile)
-              val filter = rest match {
-                case "imageContour" => new ImageContour(0xFFFFFF, 0x000000, 800000, 2)
-                case "imageKuwahara" => new ImageKuwahara(2,1)
-              }
-              val out = filter(srcBuff)
-              val destinationFile: File = new File(Boot.fileRootDestination, bodyPart.filename.orNull)
-              ImageSaver.copyBufferedImage(out, destinationFile)
-              log.info("generated! - " + destinationFile)
               data
-          }.runFold(Map.empty[String, Any])((map, tuple) => map + tuple)
-          extractedData.map(data => HttpResponse(StatusCodes.OK, entity = s"Ok. Got $data"))
+            case bodyPart: BodyPart if bodyPart.name.equals("commands") =>
+              Future[(String, String)] {
+                bodyPart.name ->
+                  bodyPart.entity.toStrict(10 seconds).value.get.get.getData().decodeString("UTF-8")
+              }
+          }.runFold(Map.empty[String, String])((map, tuple) => map + tuple)
+
+          extractedData.map(data => {
+
+            val filename: String = data.get("filename").orNull
+            val tempFile: File = new File(Boot.fileRootSource, filename)
+            val srcBuff = ImageManager.getBufferedImage(tempFile)
+            val filter = restCommand match {
+              case "imageContour" => new ImageContour(0xFFFFFF, 0x000000, 800000, 2)
+              case "imageKuwahara" => new ImageKuwahara(2, 1)
+            }
+            val out = filter(srcBuff)
+            val destinationFile: File = new File(Boot.fileRootDestination, filename)
+            ImageSaver.copyBufferedImage(out, destinationFile)
+            log.info("generated! - " + destinationFile)
+
+            HttpResponse(StatusCodes.OK, entity = s"Ok. Got $data")
+          })
             .recover {
               case ex: Exception => HttpResponse(StatusCodes.InternalServerError, entity = "Failed!")
             }
