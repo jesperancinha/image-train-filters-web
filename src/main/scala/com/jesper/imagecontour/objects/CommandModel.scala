eeb9cb1b010bb988d40bb268b54716da3809ff00
João Esperancinha <jofisaes@hotmail.com>
2016-08-28 13:38:08 +0200

    multiple filters adaptation

---
+++ src/main/scala/com/jesper/imagecontour/objects/CommandModel.scala
@@ -0,0 +1,61 @@
+package com.jesper.imagecontour.objects
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+
+/**
+  * Created by joaofilipesabinoesperancinha on 28-08-16.
+  *
+  * Please check the exaple:
+      {
+        "commands": [
+          {
+            "filter": "imageKuwahara",
+            "settings": [
+              {
+                "name": "square-size",
+                "value": 2
+              },
+              {
+                "name": "iterations",
+                "value": 2
+              }
+            ]
+          },
+          {
+            "filter": "imageContour",
+            "settings": [
+              {
+                "name": "bgColor",
+                "value": "0xFFFFFF"
+              },
+              {
+                "name": "lnColor",
+                "value": "0x000000"
+              },
+              {
+                "name": "diffThreshold",
+                "value": "800000"
+              },
+              {
+                "name": "radius",
+                "value": 2
+              }
+            ]
+          }
+        ]
+      }
+*/
+
+// domain model
+final case class SettingItem(name: String, value:String)
+final case class CommandContainer(filter:String,settings: List[SettingItem])
+final case class Commands(commands: List[CommandContainer])
+
+// collect your json format instances into a support trait:
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val settingItemFormat = jsonFormat2(SettingItem)
+  implicit val commandContainerFormat = jsonFormat2(CommandContainer)
+  implicit val commandsFormat = jsonFormat1(Commands)
+}
+
--- src/main/scala/com/jesper/imagecontour/rest/ImageContourMultiPartDataHandler.scala
+++ src/main/scala/com/jesper/imagecontour/rest/ImageContourMultiPartDataHandler.scala
@@ -1,5 +1,6 @@
 package com.jesper.imagecontour.rest
 
+import java.awt.image.BufferedImage
 import java.io.File
 
 import akka.actor.ActorSystem
@@ -12,17 +13,21 @@ import akka.stream.Materializer
 import akka.stream.scaladsl.FileIO
 import com.jesper.imagecontour.Boot
 import com.jesper.imagecontour.filters.{ImageContour, ImageKuwahara, ImageManager, ImageSaver}
+import com.jesper.imagecontour.objects.{Commands, JsonSupport}
+import net.liftweb.json.{DefaultFormats, parse}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
-trait ImageContourMultiPartDataHandler {
+trait ImageContourMultiPartDataHandler extends JsonSupport {
 
   implicit val system: ActorSystem
 
   implicit def executor: ExecutionContextExecutor
 
   implicit val materializer: Materializer
+
+  implicit val formats = DefaultFormats
 
   implicit val logSource: LogSource[AnyRef] = new LogSource[AnyRef] {
     def genString(o: AnyRef): String = o.getClass.getName
@@ -37,34 +42,44 @@ trait ImageContourMultiPartDataHandler {
           val log = Logging(system, this)
           //noinspection UnnecessaryPartialFunction
 
-          val extractedData: Future[Map[String, String]] = formData.parts.mapAsync[(String, String)](1) {
+          val extractedData: Future[Map[String, Any]] = formData.parts.mapAsync[(String, Any)](1) {
             case bodyPart: BodyPart if bodyPart.name.equals("filename") =>
               log.info(s"received ${bodyPart.name} file")
               log.info(s"command $restCommand")
               val tempFile: File = new File(Boot.fileRootSource, bodyPart.filename.orNull)
-              val data: Future[(String, String)] = bodyPart
+              val data: Future[(String, Any)] = bodyPart
                 .entity
                 .dataBytes
                 .runWith(FileIO.toFile(tempFile)).map(_ => bodyPart.name -> bodyPart.getFilename.orElse(""))
               log.info("saved! - " + tempFile)
               data
             case bodyPart: BodyPart if bodyPart.name.equals("commands") =>
-              Future[(String, String)] {
+              Future[(String, Any)] {
                 bodyPart.name ->
                   bodyPart.entity.toStrict(10 seconds).value.get.get.getData().decodeString("UTF-8")
               }
-          }.runFold(Map.empty[String, String])((map, tuple) => map + tuple)
+          }.runFold(Map.empty[String, Any])((map, tuple) => map + tuple)
 
           extractedData.map(data => {
 
-            val filename: String = data.get("filename").orNull
+            val commands: String = data.get("commands").orNull.asInstanceOf[String]
+            val jsonParser = parse(commands)
+            val commandsParsed = jsonParser.extract[Commands]
+            val filename: String = data.get("filename").orNull.asInstanceOf[String]
             val tempFile: File = new File(Boot.fileRootSource, filename)
-            val srcBuff = ImageManager.getBufferedImage(tempFile)
-            val filter = restCommand match {
-              case "imageContour" => new ImageContour(0xFFFFFF, 0x000000, 800000, 2)
-              case "imageKuwahara" => new ImageKuwahara(2, 1)
+            var srcBuff = ImageManager.getBufferedImage(tempFile)
+
+            var out: BufferedImage = null
+            for (elem <- commandsParsed.commands) {
+              val filter = elem.filter match {
+                case "imageContour" => new ImageContour(0xFFFFFF, 0x000000, 800000, 2)
+                case "imageKuwahara" => new ImageKuwahara(2, 1)
+              }
+              log.info("applied - " + elem.filter)
+              out = filter(srcBuff)
+              srcBuff = out
             }
-            val out = filter(srcBuff)
+
             val destinationFile: File = new File(Boot.fileRootDestination, filename)
             ImageSaver.copyBufferedImage(out, destinationFile)
             log.info("generated! - " + destinationFile)
