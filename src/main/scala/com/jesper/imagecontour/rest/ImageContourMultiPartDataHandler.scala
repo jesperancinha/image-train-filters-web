f77c9b7860649ac666b405e19c55c1171913b98e
Joao Esperancinha <jofisaes@gmail.com>
2018-11-06 14:59:42 +0100

    JE-00011 SIG quality particularly recursive functions

--- src/main/scala/com/jesper/imagecontour/rest/ImageContourMultiPartDataHandler.scala
+++ src/main/scala/com/jesper/imagecontour/rest/ImageContourMultiPartDataHandler.scala
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage
 import java.io.File
 
 import akka.actor.ActorSystem
-import akka.event.{LogSource, Logging}
+import akka.event.{LogSource, Logging, LoggingAdapter}
 import akka.http.scaladsl.model.Multipart.{BodyPart, FormData}
 import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
@@ -12,8 +12,8 @@ import akka.http.scaladsl.server.Route
 import akka.stream.Materializer
 import akka.stream.scaladsl.FileIO
 import com.jesper.imagecontour.Boot
-import com.jesper.imagecontour.filters.{ImageContour, ImageKuwahara, ImageManager, ImageSaver}
-import com.jesper.imagecontour.objects.{Commands, JsonSupport}
+import com.jesper.imagecontour.filters._
+import com.jesper.imagecontour.objects.{CommandContainer, Commands, JsonSupport}
 import net.liftweb.json.{DefaultFormats, parse}
 
 import scala.concurrent.duration._
@@ -27,7 +27,7 @@ trait ImageContourMultiPartDataHandler extends JsonSupport {
 
   implicit val materializer: Materializer
 
-  implicit val formats = DefaultFormats
+  implicit val formats: DefaultFormats.type = DefaultFormats
 
   implicit val logSource: LogSource[AnyRef] = new LogSource[AnyRef] {
     def genString(o: AnyRef): String = o.getClass.getName
@@ -58,48 +58,52 @@ trait ImageContourMultiPartDataHandler extends JsonSupport {
                   bodyPart.entity.toStrict(10 seconds).value.get.get.getData().decodeString("UTF-8")
               }
           }.runFold(Map.empty[String, Any])((map, tuple) => map + tuple)
-
           extractedData.map(data => {
-
             val commands: String = data.get("commands").orNull.asInstanceOf[String]
             val jsonParser = parse(commands)
             val commandsParsed = jsonParser.extract[Commands]
             val filename: String = data.get("filename").orNull.asInstanceOf[String]
             val tempFile: File = new File(Boot.fileRootSource, filename)
-            var srcBuff = ImageManager.getBufferedImage(tempFile)
-
-            var out: BufferedImage = null
-            for (elem <- commandsParsed.commands) {
-              val filter = elem.filter match {
-                case "imageContour" => new ImageContour(
-                  elem.settings.find(p => p.name.equals("bgColor")).orNull.value.toIntFromHex,
-                  elem.settings.find(p => p.name.equals("lnColor")).orNull.value.toIntFromHex,
-                  elem.settings.find(p => p.name.equals("diffThreshold")).orNull.value.toInt,
-                  elem.settings.find(p => p.name.equals("radius")).orNull.value.toInt
-                )
-                case "imageKuwahara" => new ImageKuwahara(
-                  elem.settings.find(p => p.name.equals("square-size")).orNull.value.toInt,
-                  elem.settings.find(p => p.name.equals("iterations")).orNull.value.toInt
-                )
-              }
-              log.info("applied - " + elem.filter)
-              out = filter(srcBuff)
-              srcBuff = out
-            }
-
+            val output: BufferedImage = filterBufferedImage(log, ImageManager.getBufferedImage(tempFile), commandsParsed.commands.head, commandsParsed.commands.tail)
             val destinationFile: File = new File(Boot.fileRootDestination, filename)
-            ImageSaver.copyBufferedImage(out, destinationFile)
+            ImageSaver.copyBufferedImage(output, destinationFile)
             log.info("generated! - " + destinationFile)
-
             HttpResponse(StatusCodes.OK, entity = s"Ok. Got $data")
           })
             .recover {
-              case ex: Exception => HttpResponse(StatusCodes.InternalServerError, entity = "Failed!")
+              case _: Exception => HttpResponse(StatusCodes.InternalServerError, entity = "Failed!")
             }
         }
       }
     }
   }
 
-  val routes = processMultiPartData
+  private def filterBufferedImage(log: LoggingAdapter, srcBuff: BufferedImage, elem: CommandContainer, commands: List[CommandContainer]): BufferedImage = {
+    val filter: ImageFilter[BufferedImage, BufferedImage] = createFilterFromCommandContainter(elem)
+    log.info("applied - " + elem.filter)
+    val out: BufferedImage = filter(srcBuff)
+    if (commands.isEmpty) {
+      out
+    } else {
+      filterBufferedImage(log, out, commands.head, commands.tail)
+    }
+  }
+
+  private def createFilterFromCommandContainter(elem: CommandContainer) = {
+    val filter = elem.filter match {
+      case "imageContour" => new ImageContour(
+        elem.settings.find(p => p.name.equals("bgColor")).orNull.value.toIntFromHex,
+        elem.settings.find(p => p.name.equals("lnColor")).orNull.value.toIntFromHex,
+        elem.settings.find(p => p.name.equals("diffThreshold")).orNull.value.toInt,
+        elem.settings.find(p => p.name.equals("radius")).orNull.value.toInt
+      )
+      case "imageKuwahara" => new ImageKuwahara(
+        elem.settings.find(p => p.name.equals("square-size")).orNull.value.toInt,
+        elem.settings.find(p => p.name.equals("iterations")).orNull.value.toInt
+      )
+    }
+    filter
+  }
+
+  val routes: Route = processMultiPartData
 }
