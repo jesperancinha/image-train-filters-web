b4a8988e4a23a87cd8a1d7fb5e79055461112903
Joao Esperancinha <jofisaes@gmail.com>
2021-05-08 17:05:13 +0200

    🛤 Coverage with Coveralls and Scala

--- image-train-filters-service/src/test/scala/com/jesperancinha/imagecontour/filters/chartizate/ImageChartizateGraphicsTest.scala
+++ image-train-filters-service/src/test/scala/com/jesperancinha/imagecontour/filters/chartizate/ImageChartizateGraphicsTest.scala
@@ -4,14 +4,14 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ImageChartizateGraphicsTest extends AnyFunSuite {
 
-//  test("testBgColor") {
-//    val imageChartizateGraphics: ImageChartizateGraphics = new ImageChartizateGraphics(123, 4565)
-//    assertResult(imageChartizateGraphics.bgColor) {
-//      123
-//    }
-//    assertResult(imageChartizateGraphics.densityPercentage) {
-//      4565
-//    }
-//  }
+  test("testBgColor") {
+    val imageChartizateGraphics: ImageChartizateGraphics = new ImageChartizateGraphics(123, 4565)
+    assertResult(imageChartizateGraphics.bgColor) {
+      123
+    }
+    assertResult(imageChartizateGraphics.densityPercentage) {
+      4565
+    }
+  }
 
 }
--- image-train-filters-service/src/test/scala/com/jesperancinha/imagecontour/filters/contour/ImageContourTest.scala
+++ image-train-filters-service/src/test/scala/com/jesperancinha/imagecontour/filters/contour/ImageContourTest.scala
@@ -1,44 +1,47 @@
 package com.jesperancinha.imagecontour.filters.contour
 
+import com.jesperancinha.imagecontour.filters.ImageTestUtils.getBufferedResource
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.funsuite.AnyFunSuite
 
+import java.awt.image.BufferedImage
+
 class ImageContourTest extends AnyFunSuite with BeforeAndAfterEach {
 
-  //  test("testApplyImageContour") {
-  //    val sourceImage: BufferedImage = getBufferedResource("/testMarket.jpg")
-  //    val imageContour = new ImageContour(
-  //      ImageContourConfig()
-  //        .addBgColor(0xFFFFFF)
-  //        .addLineColor(0x000000)
-  //        .addDiffThreshold(800000)
-  //        .addRadius(2),
-  //      sourceImage)
-  //    imageContour.apply()
-  //  }
+    test("testApplyImageContour") {
+      val sourceImage: BufferedImage = getBufferedResource("/testMarket.jpg")
+      val imageContour = new ImageContour(
+        ImageContourConfig()
+          .addBgColor(0xFFFFFF)
+          .addLineColor(0x000000)
+          .addDiffThreshold(800000)
+          .addRadius(2),
+        sourceImage)
+      imageContour.apply()
+    }
 
-  //  test("testApplyImageContourPanther") {
-  //    val sourceImage: BufferedImage = getBufferedResource("/testMarket.jpg")
-  //    val imageContour = new ImageContour(
-  //      ImageContourConfig()
-  //        .addBgColor(0xFFFFFF)
-  //        .addLineColor(0x000000)
-  //        .addDiffThreshold(800000)
-  //        .addRadius(2),
-  //      sourceImage)
-  //    imageContour.apply()
-  //  }
+    test("testApplyImageContourPanther") {
+      val sourceImage: BufferedImage = getBufferedResource("/testMarket.jpg")
+      val imageContour = new ImageContour(
+        ImageContourConfig()
+          .addBgColor(0xFFFFFF)
+          .addLineColor(0x000000)
+          .addDiffThreshold(800000)
+          .addRadius(2),
+        sourceImage)
+      imageContour.apply()
+    }
 
-  //  test("testApplyImageContourPanther1") {
-  //    val sourceImage: BufferedImage = getBufferedResource("/testMarketSmallBlur.png")
-  //    val imageContour = new ImageContour(
-  //      ImageContourConfig()
-  //        .addBgColor(0xFFFFFF)
-  //        .addLineColor(0x000000)
-  //        .addDiffThreshold(150000)
-  //        .addRadius(1),
-  //      sourceImage)
-  //    imageContour.apply()
-  //  }
+    test("testApplyImageContourPanther1") {
+      val sourceImage: BufferedImage = getBufferedResource("/testMarketSmallBlur.png")
+      val imageContour = new ImageContour(
+        ImageContourConfig()
+          .addBgColor(0xFFFFFF)
+          .addLineColor(0x000000)
+          .addDiffThreshold(150000)
+          .addRadius(1),
+        sourceImage)
+      imageContour.apply()
+    }
 
 }
