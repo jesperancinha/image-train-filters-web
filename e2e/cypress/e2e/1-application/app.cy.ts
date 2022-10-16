1166143068eaf69fc7918859b890296bf3b5e4e1
Joao Esperancinha <jofisaes@gmail.com>
2022-10-16 14:04:36 +0200

    Force clicks

--- e2e/cypress/e2e/1-application/app.cy.ts
+++ e2e/cypress/e2e/1-application/app.cy.ts
@@ -1,8 +1,8 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit("/")
-    cy.get('a').contains('Contour').parent().click();
-    cy.get('a').contains('Kuwahara').parent().parent().click();
-    cy.get('a').contains('Chartizate').parent().parent().click();
+    cy.get('a').contains('Contour').click({force:true});
+    cy.get('a').contains('Kuwahara').click({force:true});
+    cy.get('a').contains('Chartizate').click({force:true});
   })
 })
