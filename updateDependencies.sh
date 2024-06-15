ce810b34854b97fb985fa071737b14f055efb008
Joao Esperancinha <jofisaes@gmail.com>
2024-06-15 23:10:08 +0200

    upgrades

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -13,8 +13,8 @@ while IFS= read -r line; do
   dependency=$(echo "$current_dependency" | sed 's/\[info\] //g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\2/g' | xargs )
   echo $group
   echo $dependency
-  current_dependency_escaped=$(echo "\"$group\" %% \"$dependency\"")
-  current_dependency_escaped_single=$(echo "\"$group\" % \"$dependency\"")
-  sed -i "s/$current_dependency_escaped % \".*\"/$current_dependency_escaped % \"$new_version\"\//" build.sbt
-  sed -i "s/$current_dependency_escaped_single % \".*\"/$current_dependency_escaped_single % \"$new_version\"\//" build.sbt
+  current_dependency_escaped=$(echo "\"$group\" \%\% \"$dependency\"")
+  current_dependency_escaped_single=$(echo "\"$group\" \% \"$dependency\"")
+  sed -i "s/$current_dependency_escaped \% \".*\"/$current_dependency_escaped \% \"$new_version\"/" build.sbt
+  sed -i "s/$current_dependency_escaped_single \% \".*\"/$current_dependency_escaped_single \% \"$new_version\"/" build.sbt
 done <<< "$updates"
