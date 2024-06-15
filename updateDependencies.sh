df1c549568d7f1289a8a07888a79066da1d9b6b5
Joao Esperancinha <jofisaes@gmail.com>
2024-06-15 23:50:21 +0200

    upgrades

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -9,8 +9,8 @@ updates=$(echo "$dependency_updates" | grep ' -> ')
 while IFS= read -r line; do
   current_dependency=$(echo "$line" | awk -F' -> ' '{print $1}')
   new_version=$(echo "$line" | awk -F' -> ' '{print $2}' | xargs)
-  group=$(echo "$current_dependency" | sed 's/\[info\] //g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\1/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
-  dependency=$(echo "$current_dependency" | sed 's/\[info\] //g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\2/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
+  group=$(echo "$current_dependency" | sed 's/\[info\]//g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\1/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
+  dependency=$(echo "$current_dependency" | sed 's/\[info\]//g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\2/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
   echo $group
   echo $dependency
   current_dependency_escaped=$(echo "\"$group\" \%\% \"$dependency\"")
