659eba9cf6282772b1132025439e61e6d851f439
Joao Esperancinha <jofisaes@gmail.com>
2024-06-15 23:57:18 +0200

    upgrades

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -7,10 +7,10 @@ dependency_updates=$(sbt dependencyUpdates)
 updates=$(echo "$dependency_updates" | grep ' -> ')
 
 while IFS= read -r line; do
-  current_dependency=$(echo "$line" | awk -F' -> ' '{print $1}')
+  current_dependency=$(echo "$line" | awk -F' -> ' '{print $1}'| sed 's/\[info\]//g' | awk '{gsub(/\[info\]/, ""); print}')
   new_version=$(echo "$line" | awk -F' -> ' '{print $2}' | xargs)
-  group=$(echo "$current_dependency" | sed 's/\[info\]//g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\1/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
-  dependency=$(echo "$current_dependency" | sed 's/\[info\]//g' | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\2/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
+  group=$(echo "$current_dependency" | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\1/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
+  dependency=$(echo "$current_dependency" | sed 's/\(.*\) : .*/\1/' | sed 's/:/ \% /g' | sed 's/\(.*\) \% test/\1/' | sed 's/\([a-z.-]*\) % \([a-z.-]*\)/\2/g' | xargs | sed 's/\-/\\-/g' | sed 's/\./\\./g')
   echo $group
   echo $dependency
   current_dependency_escaped=$(echo "\"$group\" \%\% \"$dependency\"")
