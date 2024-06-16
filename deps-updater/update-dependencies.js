ff71609fc921001017b873179eab9750188730e3
João Esperancinha <jofisaes@gmail.com>
2024-06-16 19:44:08 +0200

    updates dependency script

--- deps-updater/update-dependencies.js
+++ deps-updater/update-dependencies.js
@@ -19,8 +19,11 @@ updateLines.forEach(line => {
   if(!line.indexOf("alpha") >-1) {
     const [currentDependency, newVersion] = line.split(' -> ').map(s => s.trim());
     const [organization, artifact, oldVersion] = currentDependency.split(':').map(s => s.trim());
-    if(newVersion > oldVersion) {
-      console.log(`Found organization: ${organization}, artifact: ${artifact}`);
+    console.log(`Found organization: ${organization}, artifact: ${artifact}, old version: ${oldVersion}, new possible version ${newVersion}`);
+    let numericNewVersion = Number(newVersion.split(".").map(seg => seg.padStart(4,'0')).join(''));
+    let numericOldVersion = Number(oldVersion.split(".").map(seg => seg.padStart(4,'0')).join(''));
+    console.log(`Parsed new version is ${numericNewVersion} and parsed old version is ${numericOldVersion}`);
+    if(numericNewVersion > numericOldVersion) {
       const regex = new RegExp(`("${organization}" %% "${artifact}" % ").*(",?)`);
       buildSbtContent = buildSbtContent.replace(regex, `$1${newVersion}$2`);
     }
