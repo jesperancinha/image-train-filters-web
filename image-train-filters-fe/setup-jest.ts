d0933baf2473f2dbb68e9123b99745450883ab65
jesperancinha <jofisaes@gmail.com>
2025-08-06 10:03:37 +0200

    See if this fix it

--- image-train-filters-fe/setup-jest.ts
+++ image-train-filters-fe/setup-jest.ts
@@ -1,1 +1,20 @@
-import 'jest-preset-angular/setup-jest';
+// Import the required setup from jest-preset-angular
+import 'jest-preset-angular/setup-env/zone';
+
+// Optional: Configure global mocks for Angular elements
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => ({
+    display: 'none',
+    appearance: ['-webkit-appearance']
+  })
+});
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => ({
+    enumerable: true,
+    configurable: true
+  })
+});
